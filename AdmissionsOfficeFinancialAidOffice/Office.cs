ff7a9859c6344da8e45fb37a9e84d84a06a0b594
Zihan Yang <ws704603301@gmail.com>
2022-01-29 23:24:06 -0500

    Initial Commit, Add Simulated Office

---
+++ AdmissionsOfficeFinancialAidOffice/Office.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace AdmissionsOfficeFinancialAidOffice
+{
+    public class Office
+    {
+        public void createFile(string folderPath, string date, int aNumber, int sNumber)
+        {
+            //check path
+            if (!Directory.Exists(folderPath))
+            {
+                throw new Exception("Path does not exist.");
+            }
+            //combining path for admission folder
+            string admissionFolder = Path.Combine(folderPath, "Input", "Admission", date);
+            //create folder for current date
+            Directory.CreateDirectory(admissionFolder);
+            //Instantiate random number generator
+            Random random = new Random();
+            //create admission letters
+            for (int i = 0; i < aNumber; i++)
+            {   
+                //create random ID
+                //combining the path and create file
+                string admissionLetter = Path.Combine(admissionFolder, $"admission-{random.Next(100000000).ToString("D8")}.txt");
+                using(StreamWriter sw = File.CreateText(admissionLetter))
+                {
+                    sw.WriteLine(admissionLetter);
+                }
+            }
+
+            //combining path for Scholarship folder
+            string scholarshipFolder = Path.Combine(folderPath,"Input", "Scholarship", date);
+            //create folder for current date
+            Directory.CreateDirectory(scholarshipFolder);
+            //get letter names in admission folder
+            string[] admissionLetters = Directory.GetFiles(admissionFolder);
+            for (int i=0; i<sNumber; i++)
+            {
+                // choose a random letter from admission and create scholarship letter
+                // extract the student ID
+                string studentID = extractID(admissionLetters[random.Next(aNumber)]);
+                //create schoalrship letter
+                string scholarshipLetter = Path.Combine(scholarshipFolder, $"scholarship-{studentID}.txt");
+                using(StreamWriter sw = File.CreateText(scholarshipLetter))
+                {
+                    sw.WriteLine(scholarshipLetter);
+                }
+                
+            }
+        }
+
+        //take file path and return the student ID
+        public static string extractID(string file_path)
+        {
+            return file_path.Substring(file_path.Length-12, 8);
+        }
+    }
+}
---
+++ LetterSystem/Program.cs
@@ -0,0 +1,58 @@
+﻿using AdmissionsOfficeFinancialAidOffice;
+using System.IO;
+
+//Console.WriteLine("Enter path");
+////var path1 = @"D:\Projects\LetterSystem\CombinedLetters";
+//var path = @"" + Console.ReadLine();
+////bool result = path1.Equals(path);
+////Console.WriteLine(result);
+////Console.WriteLine(path1);
+////Console.WriteLine(path);
+//var office = new Office();
+//office.createFile(path);
+
+//class TryFinallyTest
+//{
+//    static void ProcessString(string s)
+//    {
+//        if (s != null)
+//        {
+//            throw new ArgumentNullException(paramName: nameof(s), message: "parameter can't be null.");
+//        }
+//        Console.WriteLine("I'm here");
+//    }
+
+//    public static void Main()
+//    {
+//        string s = "5"; // For demonstration purposes.
+
+//        try
+//        {
+//            ProcessString(s);
+//        }
+//        catch (Exception e)
+//        {
+//            Console.WriteLine("{0} Exception caught.", e);
+//        }
+//    }
+//}
+
+//string[] fileEntries = Directory.GetFiles(".vs");
+//Random random = new Random();
+//foreach (string entry in fileEntries)
+//{
+//    int randID = random.Next(100000000);
+//    string file = Path.Combine("dsfdasdfsfsaf", $"admission-{randID.ToString("D8")}.txt");
+//    Console.WriteLine(file);
+//    Console.WriteLine(file.Substring(file.Length - 12, 8));
+//}
+
+try
+{
+    var office = new Office();
+    office.createFile("./CombinedLetters", 20220125.ToString(), 10, 5);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
